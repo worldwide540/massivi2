34403803295c6e28bba957fbd88478b21cf94f05
Нарек Антонян <narekantonyan2033@gmail.com>
2024-06-20 18:38:26 +0300

    Добавьте файлы проекта.

---
+++ massivi2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] strings = { "apple", "banana", "cherry", "date" };
+
+        Console.WriteLine("Введите строку для поиска:");
+        string input = Console.ReadLine();
+
+        bool found = false;
+        foreach (string str in strings)
+        {
+            if (str.Equals(input))
+            {
+                found = true;
+                break;
+            }
+        }
+
+        if (found)
+        {
+            Console.WriteLine("Строка найдена!");
+        }
+        else
+        {
+            Console.WriteLine("Строка не найдена!");
+        }
+    }
+}
